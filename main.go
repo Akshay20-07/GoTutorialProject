80af490eb3c0a9c83db328e9142f839349a41c34
Akshay20-07 <akshaychauhanjp@gmail.com>
2023-08-27 21:04:46 +0530

    data types and explicit casting

--- main.go
+++ main.go
@@ -4,4 +4,23 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, Gophers!")
+	datatypes()
+}
+
+func datatypes() {
+	var a string
+	a = "Akshay \n" + `b \n`
+	fmt.Print(a)
+
+	var b int = 99
+	fmt.Println(b)
+
+	var c bool = true
+	fmt.Println(c)
+
+	d := 3.14
+	fmt.Println(d)
+
+	var e int = int(d)
+	fmt.Println(e)
 }
